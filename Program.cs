4d43625057d57b18fec23f19e38d7e4a0dce4f6f
Mila <milaprivalova1@gmail.com>
2022-08-08 19:27:58 +0300

    ready1

--- Program.cs
+++ Program.cs
@@ -5,9 +5,19 @@ if( numberA>numberB)
 else
   Console.WriteLine("numberB больше");
  
+int numberC =2;
+int numberD =10;
+if( numberC>numberD)
+  Console.WriteLine("2 больше");
+else
+  Console.WriteLine("10 больше");
 
-
-
+int numberF =-9;
+int numberG =-3;
+if( numberF>numberG)
+  Console.WriteLine("-9 больше");
+else
+  Console.WriteLine("-3 больше");
 
 
 
