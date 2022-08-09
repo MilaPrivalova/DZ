e851d66eb8cddaa475ab5b2dee9acfbcabf77a13
Mila <milaprivalova1@gmail.com>
2022-08-09 19:31:52 +0300

    task3

---
+++ task_4/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = 4; 
+Console.WriteLine(n % 2 == 0 ? "Четное" : "Не четное");
+
+n = -3; 
+Console.WriteLine(n % 2 == 0 ? "Четное" : "Не четное");
+
+n = 7; 
+Console.WriteLine(n % 2 == 0 ? "Четное" : "Не четное");
