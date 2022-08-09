43fe0175db519026a22d08be4984e1158af018f5
Mila <milaprivalova1@gmail.com>
2022-08-09 18:49:39 +0300

    task2

---
+++ DZ/Program.cs
@@ -0,0 +1,26 @@
+﻿int a =2;
+int b =3;
+int c =7;
+
+int max = a;
+  if(b>max ) max = b;
+  if(c>max ) max = c;
+  Console.WriteLine(max);
+
+int f =44;
+int g =5;
+int h =78;
+
+int Max = f;
+  if(g>Max ) Max = g;
+  if(h>Max ) Max = h;
+  Console.WriteLine(max);
+
+int z =22;
+int x =3;
+int v =9;
+
+int result = z;
+  if(x>result ) result = x;
+  if(v>result ) result = v;
+  Console.WriteLine(result);
